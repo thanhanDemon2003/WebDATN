79ad58a3b593626f7f91666ff74bad129679cdde
thanhanDemon2003 <iamdemon.dev@gmail.com>
2023-11-03 00:23:53 +0700

    1

--- public/javascripts/login.js
+++ public/javascripts/login.js
@@ -1,24 +1,14 @@
-(function (d, s, id) {
-  var js,
-    fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) {
-    return;
-  }
-  js = d.createElement(s);
-  js.id = id;
-  js.src = "https://connect.facebook.net/en_US/sdk.js";
-  fjs.parentNode.insertBefore(js, fjs);
-})(document, "script", "facebook-jssdk");
-
 window.fbAsyncInit = function () {
   FB.init({
     appId: "317289147672827",
     xfbml: true,
     version: "v18.0",
   });
-  // const facebookLogin = document.getElementById("loginfbne");
-  // facebookLogin.addEventListener('click', function () {
-  //   console.log('click');
+ FB.AppEvents.logPageView();
+}
+ const facebookLogin = document.getElementById("loginfbne");
+  facebookLogin.addEventListener('click', function () {
+    console.log('click');
   FB.login(function (response) { 
     if (response.authResponse) {
       console.log("Chào Mừng Bạn! Đã Đăng Nhập. ");
@@ -36,9 +26,7 @@ window.fbAsyncInit = function () {
       console.log("Người dùng đã hủy đăng nhập.");
     }
   });
-// })
-}
-
+})
 async function loginApi(id) {
   try {
     const url =
